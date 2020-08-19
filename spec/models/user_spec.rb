a471f121cefe1cce9acab340094aa81d913b3836
keisuke-o <qiyoudaze5@gmail.com>
2020-08-19 19:40:06 +0900

    各モデルのテスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it "nameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
+      expect(@user).to be_valid
+    end
+
+    it "nameが空では登録できないこと" do
+      @user.name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "emailが空では登録できないこと" do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "passwordが空では登録できないこと" do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "passwordが6文字以上であれば登録できること" do
+      @user.password = "123456"
+      @user.password_confirmation = "123456"
+      expect(@user).to be_valid
+    end
+
+    it "passwordが5文字以下であれば登録できないこと" do
+      @user.password = "12345"
+      @user.password_confirmation = "12345"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+     expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "nameの値が存在すれば登録できること" do
+      expect(@room).to be_valid
+    end
+
+    it "nameが空では登録できないこと" do
+      @room.name = nil
+      @room.valid?
+      expect(@room.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'contentとimageが存在していれば保存できること' do
+      expect(@message).to be_valid
+    end
+
+    it 'contentが存在していれば保存できること' do
+      @message.image = nil
+      expect(@message).to be_valid
+    end
+
+    it 'imageが存在していれば保存できること' do
+      @message.content = nil
+      expect(@message).to be_valid
+    end
+
+    it 'contentとimageが空では保存できないこと' do
+      @message.content = nil
+      @message.image = nil
+      @message.valid?
+      expect(@message.errors.full_messages).to include("Content can't be blank")
+    end
+  end
+end
